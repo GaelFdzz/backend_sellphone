d8178997b1132cb814f51aed1462145a99ebbdf3
GaelFdzz <gaelandrade272@gmail.com>
2024-11-25 23:18:19 -0500

    Agregue las reseñas a los productos

--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -1,16 +1,34 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException, Post, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { ProductosService } from './productos.service';
+import { diskStorage } from 'multer';
 import * as path from 'path';
 
 @Controller('productos')
 export class ProductosController {
-  constructor(private productosService: ProductosService) {}
+  constructor(private productosService: ProductosService) { }
 
   @Get()
   async obtenerProductos() {
     return this.productosService.obtenerProductos();
+  }
+
+  @Get(':id')
+  async obtenerProductoPorId(@Param('id') id: string) {
+    console.log('ID recibido en el backend:', id);
+    // Asegúrate de convertir el id a número
+    const idNumerico = parseInt(id, 10);
+    if (isNaN(idNumerico)) {
+      throw new BadRequestException('El ID debe ser un número válido');
+    }
+
+    const producto = await this.productosService.obtenerProductoPorId(idNumerico);
+
+    if (!producto) {
+      throw new NotFoundException('Producto no encontrado');
+    }
+
+    return producto;
   }
 
   @Post('upload')
@@ -29,4 +47,15 @@ export class ProductosController {
   async subirImagen(@UploadedFile() file: any) {
     return { ruta: `/imagenes/${file.filename}` }; // Retorna la ruta pública de la imagen
   }
+
+  // productos.controller.ts
+  @Get(':id/resenas')
+  async obtenerReseñas(@Param('id') id: string) {
+    const idNumerico = parseInt(id, 10); // Convierte el parámetro string a número
+    if (isNaN(idNumerico)) {
+      throw new Error("ID de producto inválido");
+    }
+    return this.productosService.obtenerResenasPorProducto(idNumerico);
+  }
+
 }
--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductosService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async obtenerProductos() {
     return this.prisma.productos.findMany({
@@ -12,4 +12,46 @@ export class ProductosService {
       },
     });
   }
+
+  async obtenerProductoPorId(id: number) {
+    console.log('Buscando producto con ID:', id);
+
+    if (isNaN(id)) {
+      throw new Error('El ID debe ser un número válido');
+    }
+
+    const producto = await this.prisma.productos.findUnique({
+      where: {
+        Id_Producto: id,
+      },
+      include: { categorias: true, resenas: true },
+    });
+
+    if (!producto) {
+      throw new NotFoundException('Producto no encontrado');
+    }
+
+    console.log('Producto encontrado:', producto);
+    return producto;
+  }
+
+  // productos.service.ts
+  async obtenerResenasPorProducto(id: number) {
+    console.log('ID recibido:', id);
+    console.log('Tipo de ID:', typeof id);
+
+    const resenas = await this.prisma.resenas.findMany({
+      where: {
+        Id_Producto: id,
+      },
+      orderBy: {
+        Fecha: 'desc',
+      },
+    });
+
+    return resenas;
+  }
+
+
+
 }
